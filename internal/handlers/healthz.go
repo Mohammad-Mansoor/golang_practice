2838c401cde6f3565516fba397dcdf006d98ee3c
Mohammad Mansoor Safi <mansoor.safi@afghan-wireless.com>
2026-09-09 09:18:04 +0430

    initial commit

--- internal/handlers/healthz.go
+++ internal/handlers/healthz.go
@@ -9,5 +9,5 @@ func Healthz(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	log.Println("i am executed")
-	w.Write([]byte(`{"status":"ok"}`))
+	w.Write([]byte(`{"status":"okay"}`))
 }
